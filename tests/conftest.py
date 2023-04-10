3c3be391dd6076a7db000e0f1ec11b8fc177ed4a
Joonhyung Lee/이준형 <veritas9872@gmail.com>
2023-04-10 17:32:35 +0900

    Remove unnecessary import from conftest.py.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,3 @@
-import pytest
-
-
 def pytest_addoption(parser):
-    parser.addoption('--num_steps', type=int, action='store', default=64)
-    parser.addoption('--gpu', type=int, action='store', default=0)
+    parser.addoption("--num_steps", type=int, action="store", default=64)
+    parser.addoption("--gpu", type=int, action="store", default=0)
